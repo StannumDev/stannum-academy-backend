b5ce0038efe919e901a42372126e87f2d0174c4a
Mateo Lohezic <mateolohezic@gmail.com>
2023-05-03 19:19:29 -0300

    creacion estrucutra

--- model/users.js
+++ model/users.js
@@ -1,15 +1,9 @@
 const { Schema, model } = require('mongoose');
 
 const user = new Schema({
-    username: String,
     name: String,
-    surname: String,
     email: String,  
     password: String,
-    credits: Number,
-    expire: String,
-    expireUser: String,
-    parent: String,
     role: String
 })
 
